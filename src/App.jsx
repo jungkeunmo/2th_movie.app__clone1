01d55c73b26c94a72042b13ae1ccc756fecf51d8
정근모 <junggeunmo@jeong-geunmoui-MacBookAir.local>
2022-01-06 10:22:49 +0900

    movie

---
+++ src/App.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import MovieBox from "./components/movieBox";
+import axios from "axios";
+
+class App extends React.Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            movies: null,
+            currentType: 1,
+        };
+    }
+
+    _getMovieData = async() => {
+
+        const result = await axios.get(
+            "https://api.themoviedb.org/3/movie/popular?api_key=6f468133ad08eef8c7a50210567d8a1b"
+        );
+
+        console.log(result.data.results);
+        this.setState((prevState) => {
+            return {
+                movies: result.data.results,
+            };
+        });
+    };
+
+    _typeBtnClickHandler = (value) => {
+        this.setState((prevState) => {
+            return {
+                currentType:value,
+            };
+        });
+        this._getMovieData();
+    };
+
+    componentDidMount(){
+        this._getMovieData();
+    };
+
+    render() {
+        return (
+            <main className="main">
+                <div className="typeArea">
+                    <button className={this.currentType === 1 ? "active" : ""}
+                    onClick={() => this._typeBtnClickHandler(1)}
+                    > 인기영화 </button>
+                    <button className={this.currentType === 2 ? "active" : ""}
+                    onClick={() => this._typeBtnClickHandler(2)}> 현재 상영 영화 </button>
+                </div>
+                <section className="content">              
+                    {this.state.movies === null ? "LOADING" : this.state.movies.map(movie => {
+                        return <MovieBox key={movie.id} movie={movie} />
+                    })};
+                </section>
+            </main>        
+        );
+    };
+};
+
+export default App;
+
---
+++ src/components/movieBox.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+class MovieBox extends React.Component {
+    constructor(props) {
+        super(props);
+    };
+
+    render() {
+        return (
+            <section className="movieBox">
+                <section className="movieBox__left">
+                    <img src={`https://image.tmdb.org/t/p/w500${this.props.movie.poster_path}`} alt="poster" />
+                </section>
+                <section className="movieBox__right mr">
+                    <article className="mr__title">{this.props.movie.title}</article>
+                    <article className="mr__score">🐰{this.props.movie.vote_average}</article>
+                    <article className="mr__release_date">{this.props.movie.release_date}</article>
+                    <article className="mr__summary">{this.props.movie.overview}</article>
+                </section>
+            </section>
+        );
+    };
+};
+
+export default MovieBox;
